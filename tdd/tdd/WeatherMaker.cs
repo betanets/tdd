045497c806231c9cf0ab83e49bcb7d2de1557741
Александр Шкирков < >
2017-03-26 12:53:49 +0700

    Температура выдаётся и тест успешно проходит

---
+++ tdd/tdd/WeatherMaker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd
+{
+    public class WeatherMaker
+    {
+        public int getCurTemperatureByCityID(int ID)
+        {
+            return 0;
+        }
+    }
+}
