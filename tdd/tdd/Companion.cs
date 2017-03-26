e2bba1874297143f3209cd0595d5fc7814fba13e
Александр Шкирков < >
2017-03-26 15:28:05 +0700

    Теперь температура по ID города действительно выводится после запроса

--- tdd/tdd/Companion.cs
+++ tdd/tdd/Companion.cs
@@ -11,6 +11,7 @@ namespace tdd
         public string getWords(string words)
         {
             string replySymbols = ">>> ";
+            int i = -1;
             if (words == "Что ты умеешь делать?")
                 return replySymbols + "Пока ничего";
             else if (words == "Пока")
@@ -20,10 +21,19 @@ namespace tdd
                 WeatherMaker wm = new WeatherMaker();
                 return replySymbols + "Сейчас на улице: " + wm.getCurTemperatureByCityID(1510853);
             }
-            else if(words == "Покажи погоду в Новосибирске")
+            else if (words == "Покажи погоду в Новосибирске")
             {
                 WeatherMaker wm = new WeatherMaker();
                 return replySymbols + "Сейчас на улице: " + wm.getCurTemperatureByCityID(1496747);
+            }
+            else if (words == "Покажи погоду по ID города")
+            {
+                return replySymbols + "Скажешь ID города?";
+            }
+            else if (Int32.TryParse(words, out i) && i != -1)
+            {
+                WeatherMaker wm = new WeatherMaker();
+                return replySymbols + "Сейчас на улице: " + wm.getCurTemperatureByCityID(Convert.ToInt32(words));
             }
             return replySymbols + "Привет!";
         }
